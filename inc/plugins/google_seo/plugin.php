29d20691c9f4687ad571418caa244122bd1d7a9f
Andreas Klauer <Andreas.Klauer@metamorpher.de>
2010-03-24 14:40:01 +0100

    Change version to 1.1.12

--- inc/plugins/google_seo/plugin.php
+++ inc/plugins/google_seo/plugin.php
@@ -47,7 +47,7 @@ function google_seo_plugin_info()
         "description"   => $lang->googleseo_plugin_description,
         "author"        => "Andreas Klauer",
         "authorsite"    => "mailto:Andreas.Klauer@metamorpher.de",
-        "version"       => "1.1.11",
+        "version"       => "1.1.12",
         "guid"          => "8d12371391e1c95392dd567617e40f7f",
         "compatibility" => "14*,16*",
     );
