94c8eb5f0d33046c28e013c44c1b7adef28dcec2
Andreas Klauer <Andreas.Klauer@metamorpher.de>
2011-07-17 13:18:57 +0200

    Update version and compatibility to 1.6

--- inc/plugins/google_seo/plugin.php
+++ inc/plugins/google_seo/plugin.php
@@ -47,9 +47,9 @@ function google_seo_plugin_info()
         "description"   => $lang->googleseo_plugin_description,
         "author"        => "Andreas Klauer",
         "authorsite"    => "mailto:Andreas.Klauer@metamorpher.de",
-        "version"       => "1.1.13",
+        "version"       => "1.6.0",
         "guid"          => "8d12371391e1c95392dd567617e40f7f",
-        "compatibility" => "14*,16*",
+        "compatibility" => "16*",
     );
 
     // Provide some additional status information, if the plugin is enabled.
