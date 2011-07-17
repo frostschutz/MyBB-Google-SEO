5e105370be2a30d86d67f32788983251212e8ba2
Andreas Klauer <Andreas.Klauer@metamorpher.de>
2011-07-17 13:52:35 +0200

    donate button

--- inc/plugins/google_seo/plugin.php
+++ inc/plugins/google_seo/plugin.php
@@ -44,7 +44,8 @@ function google_seo_plugin_info()
 
     $info = array(
         "name"          => "Google SEO",
-        "description"   => $lang->googleseo_plugin_description,
+        "description"   => $lang->googleseo_plugin_description
+        .'<div style="float: right"><form action="https://www.paypal.com/cgi-bin/webscr" method="post"><input type="hidden" name="cmd" value="_s-xclick"><input type="hidden" name="hosted_button_id" value="8V9B4YYMJF862"><input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!"><img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1"></form></div>',
         "author"        => "Andreas Klauer",
         "authorsite"    => "mailto:Andreas.Klauer@metamorpher.de",
         "version"       => "1.6.0",
