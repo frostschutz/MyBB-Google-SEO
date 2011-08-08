4fe692c32c92ae6739a77b856b52e352d13393a4
Andreas Klauer <Andreas.Klauer@metamorpher.de>
2011-08-08 21:22:47 +0200

    GPL -> LGPL

--- inc/languages/english/admin/googleseo_plugin.lang.php
+++ inc/languages/english/admin/googleseo_plugin.lang.php
@@ -1,22 +1,4 @@
 <?php
-/**
- * This file is part of Google SEO plugin for MyBB.
- * Copyright (C) 2008, 2009 Andreas Klauer <Andreas.Klauer@metamorpher.de>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- */
 
 $l['googleseo_plugin_404'] = "404";
 $l['googleseo_plugin_apply_error'] = "Couldn't apply changes to inc/functions.php. Is the file writable?";
--- inc/languages/english/googleseo.lang.php
+++ inc/languages/english/googleseo.lang.php
@@ -1,22 +1,4 @@
 <?php
-/**
- * This file is part of Google SEO plugin for MyBB.
- * Copyright (C) 2008, 2009 Andreas Klauer <Andreas.Klauer@metamorpher.de>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- */
 
 $l['googleseo_404_notfound'] = "404 Not Found";
 $l['googleseo_404_widget'] = "<script type=\"text/javascript\">\nvar GOOG_FIXURL_LANG = 'en';\nvar GOOG_FIXURL_SITE = '{1}';\n</script>\n<script type=\"text/javascript\" src=\"http://linkhelp.clients.google.com/tbproxy/lh/wm/fixurl.js\"></script>";
