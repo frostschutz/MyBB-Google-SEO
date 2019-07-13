2723e063731b0eb81f20007598068a6b9364c237
Alfonso <vivancos@outlook.es>
2019-07-13 17:28:42 +0200

    Update googleseo.lang.php
    Use https instead of http

--- inc/languages/english/googleseo.lang.php
+++ inc/languages/english/googleseo.lang.php
@@ -1,7 +1,7 @@
 <?php
 
 $l['googleseo_404_notfound'] = "404 Not Found";
-$l['googleseo_404_widget'] = "<script type=\"text/javascript\">\nvar GOOG_FIXURL_LANG = 'en';\nvar GOOG_FIXURL_SITE = '{1}';\n</script>\n<script type=\"text/javascript\" src=\"http://linkhelp.clients.google.com/tbproxy/lh/wm/fixurl.js\"></script>";
+$l['googleseo_404_widget'] = "<script type=\"text/javascript\">\nvar GOOG_FIXURL_LANG = 'en';\nvar GOOG_FIXURL_SITE = '{1}';\n</script>\n<script type=\"text/javascript\" src=\"https://linkhelp.clients.google.com/tbproxy/lh/wm/fixurl.js\"></script>";
 $l['googleseo_404_wol'] = "Seeing an <a href=\"{1}\">Error Page</a>";
 $l['googleseo_meta_page'] = "Page";
 $l['googleseo_sitemap_disabledorinvalid'] = "Sitemap disabled or invalid.";
