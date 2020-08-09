7921701b7847aa36df6af623013aa41cd125a734
Andreas Klauer <Andreas.Klauer@metamorpher.de>
2020-08-09 13:16:15 +0200

    MySQL's 'utf8' is only a subset of UTF-8 (three byte chars). Change the database encoding warning to require 'utf8mb4' instead.

--- inc/plugins/google_seo/plugin.php
+++ inc/plugins/google_seo/plugin.php
@@ -107,8 +107,7 @@ function google_seo_plugin_status()
     }
 
     // UTF-8 is required:
-    if($mybb->config['database']['encoding'] != 'utf8'
-       && $mybb->config['database']['encoding'] != 'utf8mb4')
+    if($mybb->config['database']['encoding'] != 'utf8mb4')
     {
         $warning[] = $lang->sprintf($lang->googleseo_plugin_warn_encoding,
                                     $mybb->config['database']['encoding']);
